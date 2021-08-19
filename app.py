a07caf05ba34098b7e5e59d2b64deb81165af733
Avery.Davenport <averymdavenport@gmail.com>
2021-08-19 13:11:40 -0600

    added templates, css, and basic functions

--- app.py
+++ app.py
@@ -6,14 +6,31 @@ app = Flask(__name__)
 # make Jinja report undefined variables
 app.jinja_env.undefined = jinja2.StrictUndefined
 
+app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = True
+
 @app.route("/")
 def hello():
-    return render_template("marketing_page.html")
+    return render_template("frontpage.html")
 
 @app.route("/recipes")
-def render_recipes():
-    return render_template("recipes_page.html")
+def show_recipes():
+    """Return page showing all recipes contained in database"""
+
+    recipe_list = ['recipe', 'recipe', 'recipe','recipe','recipe','recipe','recipe','recipe','recipe','recipe']
+    return render_template("recipes.html", recipe_list=recipe_list)
+
+@app.route("/homepage")
+def show_homepage():
+    return render_template("homepage.html")
+
+@app.route("/list")
+def show_list():
+    return render_template("shopping_list.html")
+
+@app.route("/acct")
+def show_user_info():
+    return render_template("acct_info.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host="0.0.0.0")
